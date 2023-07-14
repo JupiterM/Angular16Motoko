f935ae6b13e76df04f44655e77944528e36037a7
jupes <matteoharmon@gmail.com>
2023-07-14 12:27:21 -0500

    .

---
+++ install_dfx_version.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Installing DFX Version 0.14.2..."
+DFX_VERSION=0.14.2 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    MOTOKO_CANISTER_ID: process.env.MOTOKO_CANISTER_ID,
+    MOTOKO_CANISTER_HOST: process.env.MOTOKO_CANISTER_HOST
+};
